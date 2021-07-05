e7da9f1bf1fcd8422e77e787c5b0c1c61125d048
thelscon <thelscon@gmail.com>
2021-07-05 17:13:33 +0300

    update 3

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 'use strict' ;
 
 const searchBS = 'web' ;
+let searchUserId;
 
 users.forEach ( function ( arrayUsers ) {
     
     if ( arrayUsers.company.bs.toLowerCase().includes ( searchBS.toLocaleLowerCase() ) ) {
+
+        console.log ( `Web-developer - ${arrayUsers.name}:` ) ;
         
         todos.forEach ( function ( arrayToDo ) {
             if ( arrayToDo.userId === arrayUsers.id && arrayToDo.completed === false ) {  
-                console.log ( arrayToDo.title ) ;
+                console.log ( `\t${arrayToDo.title}` ) ;
             }
         } ) ;
 
