6ac64cebaaf6d11cb6253f5b7fca42aa9a20a6a3
thelscon <thelscon@gmail.com>
2021-07-05 16:12:15 +0300

    2u

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ users.forEach ( function ( arrayUsers ) {
     if ( arrayUsers.company.bs.toLowerCase().includes ( searchBS.toLocaleLowerCase() ) ) {
         
         todos.forEach ( function ( arrayToDo ) {
-            if ( arrayToDo.userId === arrayUsers.id && !arrayToDo.completed ) {  
+            if ( arrayToDo.userId === arrayUsers.id && arrayToDo.completed === false ) {  
                 console.log ( arrayToDo.title ) ;
             }
         } ) ;
