086c694700979429fcd6b9e706b124c53e4de3bb
thelscon <thelscon@gmail.com>
2021-07-12 02:11:52 +0300

    update 8

--- script.js
+++ script.js
@@ -3,25 +3,24 @@
 const searchBS = ' web ' ;
 let searchUserId;
 
-let arrayUsers = users.filter( object =>                                                            //находим искомых пользователей
+let arrayUsers = users.filter( object =>                                                     //находим искомых пользователей
     object.company.bs.toLowerCase().includes( searchBS.toLowerCase() ) ) ; 
 
-arrayUsers.forEach( object => {                                                                     //для каждого пользователя.......... =>
+arrayUsers.forEach( object => {                                                               //для каждого найденного пользователя... =>
 
-    object.notComplete = {} ;
-
-    let notCompleted = todos.filter( objectTodos =>                                                 //находим невыполненные задачи и .............. =>
+    let notCompleted = todos.filter( objectTodos =>                                                 //находим невыполненные задачи и ... =>
         String( object.id ) === String( objectTodos.userId )  && objectTodos.completed === false 
     ) ;
 
     let key = 1 ;
-    notCompleted.forEach( objectNotCompleted => {                                                   //добавляем невыполненные задачи этому пользователю
+    object.notComplete = {} ;
+    notCompleted.forEach( objectNotCompleted => {                                 //=>...добавляем невыполненные задачи этому пользователю
         object.notComplete[ key++ ] = objectNotCompleted.title ;
     } ) ;
 
 } ) ;
 
-arrayUsers.forEach( finalUser => {                                                                  //выводим результат
+arrayUsers.forEach( finalUser => {                                                         //выводим результат
     console.log( `пользователь - ${finalUser.name}\nневыполненные задачи:` ) ;
     for( let key in finalUser.notComplete ) {
         console.log( `${key} - ${finalUser.notComplete[ key ]}` )
