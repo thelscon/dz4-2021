62ab2fbcd3c4b509eb8daf27d72a9e373fe53f25
thelscon <thelscon@gmail.com>
2021-07-22 19:12:59 +0300

    220720212

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 
 const userId = users[ users.findIndex( object => object.company.bs.includes( ' web ' ) ) ].id ;
 
-const arrayNoToDo = todos.filter( objectTodos => userId === objectTodos.userId && objectTodos.completed === false ) ;
+const arrayNoToDo = todos.filter( objectTodos => objectTodos.userId === userId && objectTodos.completed === false ) ;
 
 console.log( arrayNoToDo ) ;
